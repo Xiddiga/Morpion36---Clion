8701acd24d7da9bb153175968e5eaad294ae5a6f
mathis JAMOIS <mathis.jamois.2002@gmail.com>
2022-11-15 11:02:47 +0100

    repair git

---
+++ cmake-build-debug/CMakeFiles/Morpion36.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mathisjamois/Documents/GitHub/Morpion36---Clion/core.c" "CMakeFiles/Morpion36.dir/core.c.o" "gcc" "CMakeFiles/Morpion36.dir/core.c.o.d"
+  "/Users/mathisjamois/Documents/GitHub/Morpion36---Clion/display.c" "CMakeFiles/Morpion36.dir/display.c.o" "gcc" "CMakeFiles/Morpion36.dir/display.c.o.d"
+  "/Users/mathisjamois/Documents/GitHub/Morpion36---Clion/main.c" "CMakeFiles/Morpion36.dir/main.c.o" "gcc" "CMakeFiles/Morpion36.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Morpion36.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Morpion36.dir/core.c.o"
+  "CMakeFiles/Morpion36.dir/core.c.o.d"
+  "CMakeFiles/Morpion36.dir/display.c.o"
+  "CMakeFiles/Morpion36.dir/display.c.o.d"
+  "CMakeFiles/Morpion36.dir/main.c.o"
+  "CMakeFiles/Morpion36.dir/main.c.o.d"
+  "Morpion36"
+  "Morpion36.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Morpion36.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
